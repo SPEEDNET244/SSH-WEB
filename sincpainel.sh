7dd5cfa1cbbba98b431c36a096eda9d6f0ce7a10
SPEED NET VPN <97129588+SPEEDNET244@users.noreply.github.com>
2023-03-10 16:58:56 -0300

    Ok

--- sincpainel.sh
+++ sincpainel.sh
@@ -6,21 +6,25 @@ ln -fs /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
 clear
 echo -e "\033[1;36mPOR FAVOR AGUARDE...\033[0m"
+crack=$(cut -d"'" -f2 /opt/sshplus/licenca.txt) > /dev/null 2>&1
+sleep 1.5
 echo -e "\033[1;36mREGISTRANDO UMA LICENÇA...\033[0m"
 rm *.sh* > /dev/null 2>&1
 rm *.zip > /dev/null 2>&1
 apt install unzip -y > /dev/null 2>&1
-wget https://raw.githubusercont.com/SPEEDNET244/SSH-WEB/main/sshplus.zip.001 > /dev/null 2>&1
-wget https://raw.githubusercont.com/SPEEDNET244/SSH-WEB/main/sshplus.zip.002 > /dev/null 2>&1
-wget https://raw.githubusercont.com/SPEEDNET244/SSH-WEB/main/sshplus.zip.003 > /dev/null 2>&1
+wget https://raw.githubusercontent.com/CoutySSH/SSH-WEB/main/sshplus.zip.001 > /dev/null 2>&1
+wget https://raw.githubusercontent.com/CoutySSH/SSH-WEB/main/sshplus.zip.002 > /dev/null 2>&1
+wget https://raw.githubusercontent.com/CoutySSH/SSH-WEB/main/sshplus.zip.003 > /dev/null 2>&1
 cat sshplus.zip* > monitor.zip && zip -F monitor.zip > /dev/null 2>&1
 rm -rf /opt/sshplus > /dev/null 2>&1
 unzip monitor.zip -d  /opt/sshplus/ > /dev/null 2>&1
 chmod -R 777 /opt/sshplus > /dev/null 2>&1
 sleep 1
 if [[ -e "/opt/sshplus/licenca.txt" ]]; then
+sed -i "s;ATIVADO;$crack;g" /opt/sshplus/licenca.txt > /dev/null 2>&1
+fi
 clear
-wget https://raw.githubusercont.com/SPEEDNET244/SSH-WEB/main/sincpainel.zip > /dev/null 2>&1
+wget https://raw.githubusercontent.com/CoutySSH/SSH-WEB/main/sincpainel.zip > /dev/null 2>&1
 unzip sincpainel.zip > /dev/null 2>&1
 chmod +x *sh > /dev/null 2>&1
 service ssh restart
